c6f5b34e5d266933eb8db04f15b698d9fab9f400
Christian Kollmann <christian.kollmann@a-sit.at>
2023-10-05 12:52:15 +0200

    First shot at ASN.1 decoder

--- datatypes/src/jvmTest/kotlin/X509CertificateJvmTest.kt
+++ datatypes/src/jvmTest/kotlin/X509CertificateJvmTest.kt
@@ -1,8 +1,15 @@
-import at.asitplus.crypto.datatypes.*
+import at.asitplus.crypto.datatypes.CryptoPublicKey
+import at.asitplus.crypto.datatypes.EcCurve
+import at.asitplus.crypto.datatypes.JwsAlgorithm
+import at.asitplus.crypto.datatypes.TbsCertificate
+import at.asitplus.crypto.datatypes.X509Certificate
 import at.asitplus.crypto.datatypes.asn1.JwsExtensions.ensureSize
+import at.asitplus.crypto.datatypes.fromJcaKey
+import at.asitplus.crypto.datatypes.jcaName
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 import io.matthewnelson.encoding.base16.Base16
 import io.matthewnelson.encoding.core.Encoder.Companion.encodeToString
 import kotlinx.datetime.toKotlinInstant
@@ -18,7 +25,8 @@ import java.security.Signature
 import java.security.cert.CertificateFactory
 import java.security.interfaces.ECPublicKey
 import java.time.Instant
-import java.util.*
+import java.time.temporal.ChronoUnit
+import java.util.Date
 import kotlin.math.absoluteValue
 import kotlin.random.Random
 import kotlin.time.Duration.Companion.days
@@ -93,7 +101,6 @@ class X509CertificateJvmTest : FreeSpec({
         val ecPublicKey = keyPair.public as ECPublicKey
         val keyX = ecPublicKey.w.affineX.toByteArray().ensureSize(ecCurve.coordinateLengthBytes)
         val keyY = ecPublicKey.w.affineY.toByteArray().ensureSize(ecCurve.coordinateLengthBytes)
-        val cryptoPublicKey = CryptoPublicKey.Ec(curve = ecCurve, x = keyX, y = keyY)
 
         // create certificate with bouncycastle
         val notBeforeDate = Date.from(Instant.now())
@@ -113,16 +120,24 @@ class X509CertificateJvmTest : FreeSpec({
         val contentSigner: ContentSigner = JcaContentSignerBuilder(signatureAlgorithm.jcaName).build(keyPair.private)
         val certificateHolder = builder.build(contentSigner)
 
-        val x509Certificate = X509Certificate.deserialize(certificateHolder.encoded)
+        val x509Certificate = X509Certificate.decodeFromDer(certificateHolder.encoded)
         x509Certificate.shouldNotBeNull()
 
         x509Certificate.encodeToDer() shouldBe certificateHolder.encoded
         x509Certificate.signatureAlgorithm shouldBe signatureAlgorithm
+        x509Certificate.tbsCertificate.version shouldBe 2
         x509Certificate.tbsCertificate.issuerCommonName shouldBe commonName
         x509Certificate.tbsCertificate.subjectCommonName shouldBe commonName
-        x509Certificate.tbsCertificate.serialNumber shouldBe serialNumber
+        x509Certificate.tbsCertificate.serialNumber shouldBe serialNumber.longValueExact()
         x509Certificate.tbsCertificate.signatureAlgorithm shouldBe signatureAlgorithm
-        x509Certificate.tbsCertificate.publicKey.encoded shouldBe keyPair.public.encoded
+        x509Certificate.tbsCertificate.validFrom shouldBe notBeforeDate.toInstant().truncatedTo(ChronoUnit.SECONDS)
+            .toKotlinInstant()
+        x509Certificate.tbsCertificate.validUntil shouldBe notAfterDate.toInstant().truncatedTo(ChronoUnit.SECONDS)
+            .toKotlinInstant()
+        val parsedPublicKey = x509Certificate.tbsCertificate.publicKey
+        parsedPublicKey.shouldBeInstanceOf<CryptoPublicKey.Ec>()
+        parsedPublicKey.x shouldBe keyX
+        parsedPublicKey.y shouldBe keyY
     }
 
 })
