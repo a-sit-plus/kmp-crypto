6092295e4ef8046ff5f9bdee6d45e717d8720ece
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-05-21 16:55:10 +0200

    k2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,10 @@
 pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        maven {
-            url = uri("https://raw.githubusercontent.com/a-sit-plus/gradle-conventions-plugin/mvn/repo")
-            name = "aspConventions"
-        }
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-            name = "bigNum"
+    pluginManagement {
+        includeBuild("gradle-conventions-plugin")
+        repositories {
+            google()
+            gradlePluginPortal()
+            mavenCentral()
         }
     }
 }
