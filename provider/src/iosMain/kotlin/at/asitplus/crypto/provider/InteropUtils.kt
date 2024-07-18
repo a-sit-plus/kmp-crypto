643318bb634a9225ce2d883e05f9b8a4231be67b
Jakob Heher <jakob.heher@iaik.tugraz.at>
2024-07-18 19:39:34 +0200

    deswiftify (not taylor)

--- provider/src/iosMain/kotlin/at/asitplus/crypto/provider/InteropUtils.kt
+++ provider/src/iosMain/kotlin/at/asitplus/crypto/provider/InteropUtils.kt
@@ -5,9 +5,23 @@ package at.asitplus.crypto.provider
 import kotlinx.cinterop.*
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import platform.CoreFoundation.CFDataRef
+import platform.CoreFoundation.CFDictionaryAddValue
+import platform.CoreFoundation.CFDictionaryCreateMutable
+import platform.CoreFoundation.CFDictionaryRef
+import platform.CoreFoundation.CFErrorRefVar
+import platform.CoreFoundation.CFTypeRef
+import platform.Foundation.CFBridgingRelease
+import platform.Foundation.CFBridgingRetain
 import platform.Foundation.NSData
+import platform.Foundation.NSDictionary
 import platform.Foundation.NSError
+import platform.Foundation.NSNumber
+import platform.Foundation.NSString
 import platform.Foundation.create
+import platform.Foundation.dataWithBytes
+import platform.Security.SecCopyErrorMessageString
+import platform.darwin.OSStatus
 import platform.posix.memcpy
 
 @OptIn(ExperimentalForeignApi::class)
@@ -25,6 +39,39 @@ internal fun ByteArray.toNSData(): NSData = memScoped {
 private fun NSError.toNiceString() =
     "[Code $code] $localizedDescription\nBecause: $localizedFailureReason\nTry: $localizedRecoverySuggestion\n${if (localizedRecoveryOptions?.isEmpty() != true) "" else "Try also:\n - ${localizedRecoveryOptions!!.joinToString("\n - ")}\n"}"
 
+internal fun throwCryptoOperationFailed(thing: String, status: OSStatus) : Nothing {
+    val errorMessage = CFBridgingRelease(SecCopyErrorMessageString(status, null)) as String?
+    throw CryptoOperationFailed("Failed to $thing: [code $status] ${errorMessage ?: "unspecified security error"}")
+}
+
+class CoreFoundationException(message: String): Throwable(message)
+internal class corecall private constructor(val error: CPointer<CFErrorRefVar>) {
+    /** Helper for calling Core Foundation functions, and bridging exceptions across.
+     *
+     * Usage:
+     * ```
+     * corecall { SomeCoreFoundationFunction(arg1, arg2, ..., error) }
+     * ```
+     * `error` is provided by the implicit receiver object, and will be mapped to a
+     * `CoreFoundationException` if an error occurs.
+     */
+    companion object {
+        @OptIn(BetaInteropApi::class, ExperimentalForeignApi::class)
+        operator fun <T> invoke(call: corecall.()->T?) : T {
+            memScoped {
+                val errorH = alloc<CFErrorRefVar>()
+                val result = corecall(errorH.ptr).call()
+                val error = errorH.value
+                when {
+                    (result != null) && (error == null) -> return result
+                    (result == null) && (error != null) ->
+                        throw CoreFoundationException((CFBridgingRelease(error) as NSError).toNiceString())
+                    else -> throw IllegalStateException("Invalid state returned by Core Foundation call")
+                }
+            }
+        }
+    }
+}
 class SwiftException(message: String): Throwable(message)
 internal class swiftcall private constructor(val error: CPointer<ObjCObjectVar<NSError?>>) {
     /** Helper for calling swift-objc-mapped functions, and bridging exceptions across.
@@ -80,4 +127,21 @@ internal class swiftasync<T> private constructor(val callback: (T?, NSError?)->U
             }
         }
     }
+}
+
+internal fun MemScope.cfDictionaryOf(vararg pairs: Pair<*,*>): CFDictionaryRef {
+    val dict = CFDictionaryCreateMutable(null, pairs.size.toLong(), null, null)!!
+    defer { CFBridgingRelease(dict) } // free it after the memscope finishes
+    for (pair in pairs) {
+        val key = when(val it = pair.first) {
+            is CFTypeRef? -> it
+            else -> CFBridgingRetain(it)
+        }
+        val value = when(val it = pair.second) {
+            is CFTypeRef? -> it
+            else -> CFBridgingRetain(it)
+        }
+        CFDictionaryAddValue(dict, key, value)
+    }
+    return dict
 }
--- provider/src/iosMain/kotlin/at/asitplus/crypto/provider/os/IOSKeychainProvider.kt
+++ provider/src/iosMain/kotlin/at/asitplus/crypto/provider/os/IOSKeychainProvider.kt
@@ -7,22 +7,75 @@ import at.asitplus.crypto.datatypes.CryptoPublicKey
 import at.asitplus.crypto.datatypes.CryptoSignature
 import at.asitplus.crypto.datatypes.Digest
 import at.asitplus.crypto.datatypes.ECCurve
+import at.asitplus.crypto.datatypes.RSAPadding
 import at.asitplus.crypto.datatypes.SignatureAlgorithm
+import at.asitplus.crypto.datatypes.nativeDigest
+import at.asitplus.crypto.provider.CryptoOperationFailed
 import at.asitplus.crypto.provider.UnsupportedCryptoException
+import at.asitplus.crypto.provider.cfDictionaryOf
+import at.asitplus.crypto.provider.corecall
 import at.asitplus.crypto.provider.dsl.DSL
 import at.asitplus.crypto.provider.dsl.DSLConfigureFn
 import at.asitplus.crypto.provider.sign.SignatureInput
 import at.asitplus.crypto.provider.sign.Signer
 import at.asitplus.crypto.provider.swiftasync
 import at.asitplus.crypto.provider.swiftcall
+import at.asitplus.crypto.provider.throwCryptoOperationFailed
 import at.asitplus.crypto.provider.toByteArray
 import at.asitplus.crypto.provider.toNSData
-import at.asitplus.swift.krypto.Krypto
-import at.asitplus.swift.krypto.SignerProxy
+import kotlinx.cinterop.Arena
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.MemScope
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.reinterpret
+import kotlinx.cinterop.value
 import kotlinx.coroutines.runBlocking
+import platform.CoreFoundation.CFDataRef
+import platform.CoreFoundation.CFDictionaryCreate
+import platform.CoreFoundation.CFRelease
+import platform.Foundation.CFBridgingRelease
+import platform.Foundation.CFBridgingRetain
 import platform.Foundation.NSBundle
 import platform.Foundation.NSData
+import platform.Foundation.NSDictionary
+import platform.Foundation.create
+import platform.Security.SecCopyErrorMessageString
+import platform.Security.SecItemCopyMatching
+import platform.Security.SecKeyCopyExternalRepresentation
+import platform.Security.SecKeyCreateSignature
+import platform.Security.SecKeyGeneratePair
+import platform.Security.SecKeyIsAlgorithmSupported
+import platform.Security.SecKeyRef
+import platform.Security.SecKeyRefVar
+import platform.Security.errSecSuccess
+import platform.Security.kSecAttrApplicationTag
+import platform.Security.kSecAttrIsPermanent
+import platform.Security.kSecAttrKeySizeInBits
+import platform.Security.kSecAttrKeyType
+import platform.Security.kSecAttrKeyTypeEC
+import platform.Security.kSecAttrKeyTypeRSA
+import platform.Security.kSecAttrLabel
+import platform.Security.kSecAttrTokenID
+import platform.Security.kSecAttrTokenIDSecureEnclave
+import platform.Security.kSecClass
+import platform.Security.kSecClassKey
+import platform.Security.kSecKeyOperationTypeSign
+import platform.Security.kSecPrivateKeyAttrs
+import platform.Security.kSecPublicKeyAttrs
+import platform.Security.kSecReturnRef
+import secKeyAlgorithm
+
+private object KeychainTags {
+    private val tags by lazy {
+        val bundleId = NSBundle.mainBundle.bundleIdentifier
+            ?: throw UnsupportedCryptoException("Keychain access is unsupported outside of a Bundle")
+        Pair("kmp-crypto-privatekey-$bundleId", "kmp-crypto.publickey-$bundleId")
+    }
+    val PRIVATE_KEYS get() = tags.first
+    val PUBLIC_KEYS get() = tags.second
+}
 
 class iosSigningKeyConfiguration internal constructor(): PlatformSigningKeyConfiguration<iosSignerConfiguration>() {
 
@@ -32,37 +85,128 @@ class iosSignerConfiguration internal constructor(): SignerConfiguration() {
 
 }
 
-class iosEnclaveSigner (private val proxy: SignerProxy, override val signatureAlgorithm: SignatureAlgorithm.ECDSA): Signer.Unlocked, Signer.ECDSA {
-    override val publicKey = swiftcall { proxy.getPublicKeyAndReturnError(error) }.let(NSData::toByteArray).let {
-        CryptoPublicKey.EC.fromAnsiX963Bytes(ECCurve.SECP_256_R_1, it)
+sealed class unlockedIOSSigner(private val ownedArena: Arena, private val privateKeyRef: SecKeyRef) : Signer.UnlockedHandle {
+    abstract val parent: iosSigner<*>
+    val alias get() = parent.alias
+
+    var usable = true
+    final override fun close() {
+        if (!usable) return
+        usable = false
+        ownedArena.clear()
     }
 
-    override fun sign(data: SignatureInput): KmmResult<CryptoSignature> = catching {
-        require(data.format == null) { "Pre-hashed data is unsupported on iOS" }
-        val sigBytes = swiftcall {
-            proxy.signAndReturnError(
-                signatureAlgorithm.digest.toString(),
-                data.data.fold(byteArrayOf(), ByteArray::plus).toNSData(),
-                error)
+    internal fun checkSupport() {
+        if (!SecKeyIsAlgorithmSupported(privateKeyRef, kSecKeyOperationTypeSign, signatureAlgorithm.secKeyAlgorithm)) {
+            close()
+            throw UnsupportedCryptoException("Requested operation is not supported by this key")
         }
-        CryptoSignature.EC.decodeFromDer(sigBytes).withCurve(ECCurve.SECP_256_R_1)
+    }
+
+    protected abstract fun bytesToSignature(sigBytes: ByteArray): CryptoSignature
+    override fun sign(data: SignatureInput): KmmResult<CryptoSignature> = catching {
+        if (!usable) throw IllegalStateException("Scoping violation; using key after it has been freed")
+        require(data.format == null) { "Pre-hashed data is unsupported on iOS" }
+        val algorithm = signatureAlgorithm.secKeyAlgorithm
+        val signatureBytes = corecall {
+            SecKeyCreateSignature(privateKeyRef, algorithm, CFBridgingRetain(data.data) as? CFDataRef, error)
+        }.let { (CFBridgingRelease(it) as NSData).toByteArray() }
+        return@catching bytesToSignature(signatureBytes)
+    }
+
+    class ECDSA(ownedArena: Arena,
+                privateKeyRef: SecKeyRef,
+                override val parent: iosSigner.ECDSA)
+                : unlockedIOSSigner(ownedArena, privateKeyRef), Signer.ECDSA
+    {
+        override val signatureAlgorithm get() = parent.signatureAlgorithm
+        override val publicKey get() = parent.publicKey
+        override fun bytesToSignature(sigBytes: ByteArray) =
+            CryptoSignature.EC.decodeFromDer(sigBytes).withCurve(publicKey.curve)
+    }
+
+    class RSA(ownedArena: Arena,
+              privateKeyRef: SecKeyRef,
+              override val parent: iosSigner.RSA)
+              : unlockedIOSSigner(ownedArena, privateKeyRef), Signer.RSA
+    {
+        override val signatureAlgorithm get() = parent.signatureAlgorithm
+        override val publicKey get() = parent.publicKey
+        override fun bytesToSignature(sigBytes: ByteArray): CryptoSignature {
+            TODO("Not yet implemented")
+        }
+    }
+
+}
+
+sealed class iosSigner<H : unlockedIOSSigner>(
+    val alias: String,
+    private val config: iosSignerConfiguration
+) : Signer.TemporarilyUnlockable<H>() {
+
+    override suspend fun unlock(): KmmResult<H> = catching {
+        val arena = Arena()
+        val privateKey = arena.alloc<SecKeyRefVar>()
+        try {
+            memScoped {
+                val query = cfDictionaryOf(
+                    kSecClass to kSecClassKey,
+                    kSecAttrLabel to alias,
+                    kSecAttrApplicationTag to KeychainTags.PRIVATE_KEYS,
+                    kSecAttrKeyType to when (this@iosSigner) {
+                        is ECDSA -> kSecAttrKeyTypeEC
+                        is RSA -> kSecAttrKeyTypeRSA
+                    },
+                    kSecReturnRef to true,
+                )
+                val status = SecItemCopyMatching(query, privateKey.ptr.reinterpret())
+                if ((status == errSecSuccess) && (privateKey.value != null)) {
+                    return@memScoped /* continue below try/catch */
+                } else {
+                    throwCryptoOperationFailed("retrieve private key", status)
+                }
+            }
+        } catch (e: Throwable) {
+            arena.clear()
+            throw e
+        }
+        /* if the block did not throw, the handle takes ownership of the arena */
+        toUnlocked(arena, privateKey.value!!)
+    }
+
+    protected abstract fun toUnlocked(arena: Arena, key: SecKeyRef): H
+    class ECDSA(alias: String, config: iosSignerConfiguration,
+                override val publicKey: CryptoPublicKey.EC)
+                : iosSigner<unlockedIOSSigner.ECDSA>(alias, config), Signer.ECDSA
+    {
+        override val signatureAlgorithm = when (val digest = if (config.ec.v.digestSpecified) config.ec.v.digest else publicKey.curve.nativeDigest){
+            Digest.SHA256, Digest.SHA384, Digest.SHA512 -> SignatureAlgorithm.ECDSA(digest, publicKey.curve)
+            else -> throw UnsupportedCryptoException("ECDSA with $digest is not supported on iOS")
+        }
+
+        override fun toUnlocked(arena: Arena, key: SecKeyRef) =
+            unlockedIOSSigner.ECDSA(arena, key, this)
+    }
+
+    class RSA(alias: String, config: iosSignerConfiguration,
+                override val publicKey: CryptoPublicKey.Rsa)
+                : iosSigner<unlockedIOSSigner.RSA>(alias, config), Signer.RSA
+    {
+        override val signatureAlgorithm = SignatureAlgorithm.RSA(
+            digest = if (config.rsa.v.digestSpecified) config.rsa.v.digest else Digest.SHA512,
+            padding = if (config.rsa.v.paddingSpecified) config.rsa.v.padding else RSAPadding.PSS)
+
+        override fun toUnlocked(arena: Arena, key: SecKeyRef) =
+            unlockedIOSSigner.RSA(arena, key, this)
     }
 }
-@OptIn(ExperimentalForeignApi::class)
-object IOSKeychainProvider:  TPMSigningProviderI<iosEnclaveSigner, iosSignerConfiguration, iosSigningKeyConfiguration> {
 
-    private fun getSignerFromProxy(proxy: SignerProxy, config: iosSignerConfiguration): iosEnclaveSigner {
-        val ecConfig = config.ec.v
-        val digest = if (ecConfig.digestSpecified) ecConfig.digest else Digest.SHA256
-        when (digest) {
-            Digest.SHA256, Digest.SHA384, Digest.SHA512 -> {}
-            else -> throw UnsupportedCryptoException("Requested digest $digest is unsupported on iOS")
-        }
-    }
+@OptIn(ExperimentalForeignApi::class)
+object IOSKeychainProvider:  TPMSigningProviderI<iosSigner<*>, iosSignerConfiguration, iosSigningKeyConfiguration> {
     override fun createSigningKey(
         alias: String,
         configure: DSLConfigureFn<iosSigningKeyConfiguration>
-    ): KmmResult<iosEnclaveSigner> = catching {
+    ): KmmResult<iosSigner<*>> = catching {
         val config = DSL.resolve(::iosSigningKeyConfiguration, configure)
         val ecConfig = when (val it = config._algSpecific.v) {
             is SigningKeyConfiguration.ECConfiguration -> it
@@ -71,18 +215,66 @@ object IOSKeychainProvider:  TPMSigningProviderI<iosEnclaveSigner, iosSignerConf
         if (ecConfig.curve != ECCurve.SECP_256_R_1) {
             throw UnsupportedCryptoException("The iOS secure enclave only supports ECDSA keys on P-256")
         }
-        swiftcall {
-            Krypto.createAttestedP256Key(alias, error)
-        }.let {
-            getSignerFromProxy(it, DSL.resolve(::iosSignerConfiguration, config.signer.v))
+
+        val publicKeyBytes: ByteArray = memScoped {
+            val attr = cfDictionaryOf(
+                kSecAttrKeyType to kSecAttrKeyTypeEC,
+                kSecAttrKeySizeInBits to 256,
+                kSecAttrTokenID to kSecAttrTokenIDSecureEnclave,
+                kSecPrivateKeyAttrs to cfDictionaryOf(
+                    kSecAttrLabel to alias,
+                    kSecAttrIsPermanent to true,
+                    kSecAttrApplicationTag to KeychainTags.PRIVATE_KEYS
+                ),
+                kSecPublicKeyAttrs to cfDictionaryOf(
+                    kSecAttrLabel to alias,
+                    kSecAttrIsPermanent to false,
+                    kSecAttrApplicationTag to KeychainTags.PUBLIC_KEYS
+                )
+            )
+            val pubkey = alloc<SecKeyRefVar>()
+            val privkey = alloc<SecKeyRefVar>()
+            val status = SecKeyGeneratePair(attr, pubkey.ptr, privkey.ptr)
+            if ((status == errSecSuccess) && (pubkey.value != null) && (privkey.value != null)) {
+                return@memScoped corecall {
+                    SecKeyCopyExternalRepresentation(pubkey.value, error)
+                }.let { CFBridgingRelease(it) as NSData }.toByteArray()
+            } else {
+                throwCryptoOperationFailed("generate key", status)
+            }
         }
+        iosSigner.ECDSA(alias,
+            DSL.resolve(::iosSignerConfiguration, config.signer.v),
+            CryptoPublicKey.EC.fromAnsiX963Bytes(ECCurve.SECP_256_R_1, publicKeyBytes))
+
     }
 
     override fun getSignerForKey(
         alias: String,
         configure: DSLConfigureFn<iosSignerConfiguration>
-    ): KmmResult<iosEnclaveSigner> {
-        TODO("Not yet implemented")
+    ): KmmResult<iosSigner<*>> = catching {
+        val config = DSL.resolve(::iosSignerConfiguration, configure)
+        val publicKeyBytes: ByteArray = memScoped {
+            val publicKey = alloc<SecKeyRefVar>()
+            val query = cfDictionaryOf(
+                kSecClass to kSecClassKey,
+                kSecAttrLabel to alias,
+                kSecAttrApplicationTag to KeychainTags.PUBLIC_KEYS,
+                kSecReturnRef to true,
+            )
+            val status = SecItemCopyMatching(query, publicKey.ptr.reinterpret())
+            if ((status == errSecSuccess) && (publicKey.value != null)) {
+                return@memScoped corecall {
+                    SecKeyCopyExternalRepresentation(publicKey.value, error)
+                }.let { CFBridgingRelease(it) as NSData }.toByteArray()
+            } else {
+                throwCryptoOperationFailed("retrieve public key", status)
+            }
+        }
+        return@catching when (val publicKey = CryptoPublicKey.fromIosEncoded(publicKeyBytes)) {
+            is CryptoPublicKey.EC -> iosSigner.ECDSA(alias, config, publicKey)
+            is CryptoPublicKey.Rsa -> iosSigner.RSA(alias, config, publicKey)
+        }
     }
 
     override fun deleteSigningKey(alias: String) {
